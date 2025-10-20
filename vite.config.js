d049b9eb834393b90b220d6d029e3cdf25b94ebd
cristian <cristian.anghel4@gmail.com>
2025-10-20 20:39:30 +0300

    add root back to vite config and change path source for gcs bucket

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     base: './',
+    root: "src",
     build: {
-        outDir: "../dist",
+        outDir: "./dist",
     }
 })
