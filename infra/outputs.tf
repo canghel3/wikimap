47c10fa90b20723dc406ed2869d7f406f69cd780
cristian <cristian.anghel4@gmail.com>
2025-11-09 18:51:09 +0200

    echo mediawiki uri

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,6 +3,11 @@ output "gateway_url" {
   value       = google_cloud_run_v2_service.gateway_service.uri
 }
 
+output "mediawiki_uri" {
+  description = "The uri of mediawiki"
+  value       = google_cloud_run_v2_service.mediawiki_service.uri
+}
+
 output "frontend_base_url" {
   description = "Public base url for frontend assets"
   value = "https://storage.googleapis.com/${google_storage_bucket.frontend_bucket.name}/"
