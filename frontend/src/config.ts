07da24d2c3234656a38dc3569e7fb64e98cc3d60
cristian <cristian.anghel4@gmail.com>
2025-11-09 17:48:20 +0200

    prefix frontend env var with VITE_

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,12 +1,12 @@
 const config = {
     apiUrl: import.meta.env.VITE_API_URL as string,
-    baseUrl: import.meta.env.FE_BASE_URL as string,
+    baseUrl: import.meta.env.VITE_FE_BASE_URL as string,
 };
 
 // Validate that all required variables are present
 for (const [key, value] of Object.entries(config)) {
     if (value === undefined) {
-        throw new Error(`Missing environment variable for config key: ${key}`);
+        console.warn(`Missing environment variable for config key: ${key}`)
     } else {
         console.log(key, value);
     }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-const FE_BASE_URL = process.env.FE_BASE_URL || ''
+const FE_BASE_URL = process.env.VITE_FE_BASE_URL || ''
 
 // https://vite.dev/config/
 export default defineConfig({
